436209be5aac432979bc03b61c63629f6eb0d0aa
Yusniel Brocard <brocard@gmail.com>
2023-11-13 10:39:32 -0600

    first commit

---
+++ components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { myLayer } = useAppConfig()
+</script>
+
+<template>
+  <div class="space-y-1.5">
+    <h1>Hello World!</h1>
+    <pre class="text-red-700 text-xs">{{ myLayer }}</pre>
+  </div>
+</template>
