e60bd3d1f453a062863b8f3c13213f09e9dfabc6
Yusniel Brocard <brocard@gmail.com>
2023-11-17 18:46:50 -0600

    update layer repo

--- app.config.ts
+++ app.config.ts
@@ -1,61 +1,62 @@
 export default defineAppConfig({
-  myLayer: {
-    name: 'Hello from Nuxt layer'
+  uiLayer: {
+    name: "Hello from Nuxt layer",
   },
   ui: {
     variables: {
       light: {
-        background: '255 255 255',
-        foreground: 'var(--color-gray-700)'
+        background: "255 255 255",
+        foreground: "var(--color-gray-700)",
       },
       dark: {
-        background: 'var(--color-gray-900)',
-        foreground: 'var(--color-gray-200)'
+        background: "var(--color-gray-900)",
+        foreground: "var(--color-gray-200)",
       },
       header: {
-        height: '4rem'
-      }
+        height: "4rem",
+      },
     },
     icons: {
-      dark: 'i-heroicons-moon-20-solid',
-      light: 'i-heroicons-sun-20-solid',
-      search: 'i-heroicons-magnifying-glass-20-solid',
-      external: 'i-heroicons-arrow-up-right-20-solid',
-      chevron: 'i-heroicons-chevron-down-20-solid',
-      hash: 'i-heroicons-hashtag-20-solid'
+      dark: "i-heroicons-moon-20-solid",
+      light: "i-heroicons-sun-20-solid",
+      search: "i-heroicons-magnifying-glass-20-solid",
+      external: "i-heroicons-arrow-up-right-20-solid",
+      chevron: "i-heroicons-chevron-down-20-solid",
+      hash: "i-heroicons-hashtag-20-solid",
     },
     presets: {
       button: {
         primary: {
-          color: 'white',
-          variant: 'solid'
+          color: "white",
+          variant: "solid",
         },
         secondary: {
-          color: 'gray',
-          variant: 'ghost'
+          color: "gray",
+          variant: "ghost",
         },
         input: {
-          color: 'white',
-          variant: 'outline',
+          color: "white",
+          variant: "outline",
           ui: {
-            font: '',
+            font: "",
             color: {
               white: {
-                outline: 'ring-1 ring-inset ring-gray-300 dark:ring-gray-700 hover:ring-gray-300 dark:hover:ring-gray-700 hover:bg-gray-100/50 dark:hover:bg-gray-800/50 text-gray-400 dark:text-gray-500 hover:text-gray-700 dark:hover:text-gray-200 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible-ring-primary-400'
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-})
+                outline:
+                  "ring-1 ring-inset ring-gray-300 dark:ring-gray-700 hover:ring-gray-300 dark:hover:ring-gray-700 hover:bg-gray-100/50 dark:hover:bg-gray-800/50 text-gray-400 dark:text-gray-500 hover:text-gray-700 dark:hover:text-gray-200 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible-ring-primary-400",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
-declare module '@nuxt/schema' {
+declare module "@nuxt/schema" {
   interface AppConfigInput {
-    myLayer?: {
+    uiLayer?: {
       /** Project name */
-      name?: string
-    }
+      name?: string;
+    };
   }
 }
