6d52152b95bd2e8a37b7e1a1070b30ab8ad2b3c7
Yusniel Brocard <brocard@gmail.com>
2023-11-22 11:40:37 -0600

    feat: Add ColorModeToggle component, remove links/[id] page

---
+++ components/color-mode/ColorModeToggle.vue
@@ -0,0 +1,30 @@
+<template>
+  <ClientOnly>
+    <UToggle
+      v-model="isDark"
+      :on-icon="appConfig.ui.icons.dark"
+      :off-icon="appConfig.ui.icons.light"
+      :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
+    />
+
+    <template #fallback>
+      <div class="h-8 w-8" />
+    </template>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+  const colorMode = useColorMode();
+  const appConfig = useAppConfig();
+
+  // Computed
+
+  const isDark = computed({
+    get() {
+      return colorMode.value === "dark";
+    },
+    set() {
+      colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+    },
+  });
+</script>
