05cd5ca01a3658cb9e8e2c4456cb5c37604c69bc
Yusniel Brocard <brocard@gmail.com>
2023-11-23 10:15:04 -0600

    updates packages

--- .playground/pages/index.vue
+++ .playground/pages/index.vue
@@ -1,10 +1,46 @@
-<script lang="ts" setup></script>
+<script setup>
+  const links = [
+    {
+      label: "DocsSearch",
+      to: "/pro/components/docs/docs-search",
+      icon: "i-heroicons-document-magnifying-glass",
+      children: [
+        {
+          label: "DocsSearchButton",
+          to: "/pro/components/docs/docs-search-button",
+          badge: "New",
+        },
+        {
+          label: "DocsSearchInput",
+          to: "/pro/components/docs/docs-search-input",
+        },
+        {
+          label: "DocsSearchResults",
+          to: "/pro/components/docs/docs-search-results",
+        },
+      ],
+    },
+    {
+      label: "DocsSearchButton",
+      to: "/pro/components/docs/docs-search-button",
+      icon: "i-heroicons-magnifying-glass",
+      badge: "New",
+    },
+    {
+      label: "DocsSurround",
+      to: "/pro/components/docs/docs-surround",
+      icon: "i-heroicons-arrows-right-left",
+    },
+    {
+      label: "DocsToc",
+      to: "/pro/components/docs/docs-toc",
+      icon: "i-heroicons-list-bullet",
+    },
+  ];
+</script>
 
 <template>
-  <div class="container mx-auto p-10">
-    <ColorModeButton />
-    <ColorModeToggle />
+  <div class="container mx-auto py-20">
+    <NavigationTree :links="links" />
   </div>
 </template>
-
-<style scoped></style>
