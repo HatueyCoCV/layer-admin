ac447e15d1e9380a4d83bc488acbb9532d3007c8
Yusniel Brocard <brocard@gmail.com>
2023-11-23 05:44:08 -0600

    wip

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-const { myLayer } = useAppConfig()
+  const { uiLayer } = useAppConfig();
 </script>
 
 <template>
   <div class="space-y-1.5">
     <h1>Hello World!</h1>
-    <pre class="text-red-700 text-xs">{{ myLayer }}</pre>
+    <pre class="text-xs text-red-700">{{ uiLayer }}</pre>
   </div>
 </template>
